84f5eaa4c67cc1f473226ca13aeed15c803492cc
salujajaskeerat <salujajaskeerat@gmail.com>
2020-03-21 17:17:03 +0530

    Detail site added succesfully

--- profreview/review/urls.py
+++ profreview/review/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+app_name='review'
 urlpatterns=[
-	url((r'^$'),views.index,name='index'),
+	url((r'^$'),views.IndexView.as_view(),name='index'),
+	url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='details'),
 	]
